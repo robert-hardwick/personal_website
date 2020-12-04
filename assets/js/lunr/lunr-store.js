cedc87c5d26247498a032dcaccd94b324c8815d7
Robert Hardwick <robertmhardwick@gmail.com>
2020-12-04 10:52:50 +0000

    Updated site with fixmatch

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2020/11/28/welcome-to-jekyll.html",
+        "title": "Deep Learning : Hello World",
+        "excerpt":"The internet regards the MNIST dataset as a sort-of “hello world” in the deep learning world so it seem’s a good starting point for getting to know the basics of various deep learning techniques and tools. Problem Description The MNIST dataset is a well-known collection of images of handwritten digits...","categories": ["ml"],
+        "tags": ["deep learning","machine learning"],
+        "url": "http://localhost:4000/ml/2017/10/31/hello-world.html",
+        "teaser": null
+      },{
+        "title": "Building a Deep Learning PC without breaking the bank",
+        "excerpt":"Whilst participating in a kaggle competition to identify dog breeds from images of dogs I found myself using an AWS GPU Compute service in order to train the deep neural network, however I quickly found the costs mounting up as i’d often be tweaking the code and paying for the...","categories": ["ml"],
+        "tags": ["deep learning","machine learning"],
+        "url": "http://localhost:4000/ml/2017/12/18/cpu-vs-gpu.html",
+        "teaser": null
+      },{
+        "title": "FixMatch - A Simple yet Effective approach to Semi Supervised Learning",
+        "excerpt":"Deep Neural Networks can deliver impressive performance for a variety of computer vision tasks. The key to such impressive performance is in allowing the model to ingest a very large amount of labelled data during the training stage; a process known as Supervised Learning. This learning technique, whilst unbeatable in...","categories": ["ml"],
+        "tags": ["semi-supervised learning","SSL","machine learning"],
+        "url": "http://localhost:4000/ml/2020/11/29/fixmatch.html",
         "teaser": null
       }]
